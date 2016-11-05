d2772267b5c277017c2ee98b8f55ae744d777882
ngryman <ngryman@gmail.com>
2016-11-05 19:34:02 +0100

    :beetle: fix not specified region

--- functions/riot-proxy.js
+++ functions/riot-proxy.js
@@ -3,7 +3,10 @@ var request = require('request')
 var apiDomain = 'api.pvp.net'
 
 module.exports = function(context, cb) {
-  var targetUrl = `https://${context.data.region}.${apiDomain}/${context.data.url}?api_key=${context.secrets.apiKey}`
+  // support 0.1.0
+  var region = context.data.region || 'euw'
+
+  var targetUrl = `https://${region}.${apiDomain}/${context.data.url}?api_key=${context.secrets.apiKey}`
 
   request.get({ url: targetUrl }, function(err, res, body) {
     if (err) return cb(err)
