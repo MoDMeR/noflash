35dd0139d8a02dc4de195608a8a1b5c1ff6befc3
Nicolas Gryman <ngryman@gmail.com>
2016-11-14 17:15:13 +0100

    :beetle: fix undefined region

--- functions/riot-proxy.js
+++ functions/riot-proxy.js
@@ -5,8 +5,9 @@ var apiDomain = 'api.pvp.net'
 module.exports = function(context, cb) {
   // support 0.1.0
   var region = context.data.region || 'euw'
-
-  var targetUrl = `https://${region}.${apiDomain}/${context.data.url}?api_key=${context.secrets.apiKey}`
+  var url = context.data.url.replace('undefined', 'euw')
+  
+  var targetUrl = `https://${region}.${apiDomain}/${url}?api_key=${context.secrets.apiKey}`
 
   request.get({ url: targetUrl }, function(err, res, body) {
     if (err) return cb(err)
